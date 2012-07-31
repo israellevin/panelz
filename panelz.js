5c57c656aa494cc5e547a7b447157bf032e3fcac
root <root@magnum.octarine>
2012-07-31 04:13:43 +0300

    Edit story

--- panelz.js
+++ panelz.js
@@ -413,7 +413,7 @@ $(function(){
         text().split("\n");
 
     // Lastly we forward the story to a hard coded bookmark, so we don't have to page from the beginning every time. TODO In the future, this value will be taken from a cookie, or the cursor position in the textarea. Maybe it will even get its own global object.
-    for(var x = 0; x < 1; (x++)) Canvas.go(1);
+    for(var x = 0; x < 30; (x++)) Canvas.go(1);
 
 // And we bind the keyboard driven interface.
 }).keydown(function(e){
